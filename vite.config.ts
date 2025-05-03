96082ec1c7730792a15765ea5c03bbbd82d00bd1
Anik Tripathi <42141164-nick7trip@users.noreply.replit.com>
2025-05-03 19:33:42 +0000

    Fix: Added correct base path to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-
 export default defineConfig({
+  base: "/Aeonark/", // 👈 Add this line
   plugins: [
     react(),
     runtimeErrorOverlay(),
